4efc4b452fe857180f4c1733f7bfca0dbc4b9229
Michael Yarichuk <michael.yarichuk@gmail.com>
2025-07-18 23:59:42 +0300

    chore: do not capture *all* references in the lambda (fix maintainability issue)

--- src/HostBuilder.cpp
+++ src/HostBuilder.cpp
@@ -3,6 +3,29 @@
 #include <boost/asio.hpp>
 
 namespace generic_host {
+
+    Host::Host(std::vector<std::shared_ptr<IHostedService>> services)
+        : _services(std::move(services)) {}
+
+    int Host::Run() const {
+        boost::asio::io_context io;
+        boost::asio::signal_set sigs(io, SIGINT, SIGTERM);
+
+        sigs.async_wait([&io](auto, int) {
+            io.stop();
+        });
+
+        for (auto &svc: _services)
+            svc->Start(io);
+
+        io.run(); // blocks; CTRL-C calls stop()
+
+        for (auto &svc: _services)
+            svc->Stop();
+
+        return 0;
+    }
+
     HostBuilder& HostBuilder::ConfigureServices(std::function<void(std::vector<std::shared_ptr<IHostedService>>&)> configure) {
         configure(_services);
         return *this;
