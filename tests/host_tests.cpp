beecaa1421fee8b6fed83ed2526a6f21682056b1
Michael Yarichuk <michael.yarichuk@gmail.com>
2025-07-20 00:29:12 +0300

    feat: implement DI resolver (to abstract away boost::di and make stuff more like .net)

--- tests/host_tests.cpp
+++ tests/host_tests.cpp
@@ -19,13 +19,13 @@ TEST_CASE("Host should successfully start and stops services") {
     auto service = std::make_shared<DummyService>();
 
     HostBuilder builder;
-    builder.ConfigureServices([&](auto& services) {
+    builder.ConfigureServices([&service](auto& services) {
         services.push_back(service);
     });
 
     auto host = builder.Build();
 
-    std::thread t([&]() {
+    std::jthread t([] {
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
         std::raise(SIGINT); // Simulate Ctrl+C
     });
